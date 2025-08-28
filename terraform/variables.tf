06ed5db426f9cc45b53c8fa9811a0fa8c4abe532
JEBDEv <belga.justin.evan@gmail.com>
2025-08-28 15:59:03 +0800

    conf: adding adding base network and bastion variables

---
+++ terraform/variables.tf
@@ -0,0 +1,49 @@
+variable "vcp_cidr" {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  type = string
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  type = string
+  default = "10.0.2.0/24"
+}
+
+variable "availability_zone" {
+  type = string
+  default = "ap-southeast-1a"
+}
+
+variable "key_name" {
+  type = string
+  default = "cinch-test-key"
+}
+
+variable "create_nat_instance" {
+  type = bool
+  default = true
+}
+
+variable "nat_instance_ami_id" {
+  type = string
+  default = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64" # amazon linux 2023
+}
+
+variable "allowed_ssh_cidr" {
+  type = string
+  default = "" # IP Address of DevOps
+}
+
+variable "bastion_instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "bastion_instance_ami_id" {
+  type = string
+  default = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2" # amazon linux 2
+}
